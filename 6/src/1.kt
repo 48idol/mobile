e47820465d3fcb8b539cf01968bdb3892584d941
48idol <akulov_yan06@mail.ru>
2024-10-08 22:36:43 +0900

    Add files via upload

---
+++ 6/src/1.kt
@@ -0,0 +1,6 @@
+fun main(){
+    val mas: Array<Int> = arrayOf(13542,656,43,992,73)
+    for (i in mas){
+        println(i)
+    }
+}
---
+++ 6/src/2.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val mass = arrayOf(1254, 3567, 304, 862, 8357)
+    var sum = 0
+    for (i in mass) {
+        sum += i
+    }
+    print(sum)
+}
